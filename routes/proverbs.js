0c525ce4ec2c1fb27a77ad7c37acd514166c8240
Morris Oliha <niyemayo@gmail.com>
2026-03-12 06:23:40 +0100

    Add Swagger documentation for routes

--- routes/proverbs.js
+++ routes/proverbs.js
@@ -1,13 +1,86 @@
-
 const express = require("express");
 const router = express.Router();
 
 const proverbsController = require("../controllers/proverbs");
 
+/**
+ * @swagger
+ * /proverbs:
+ *   get:
+ *     summary: Retrieve all Nigerian proverbs
+ *     description: Returns a list of all proverbs stored in the database
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved proverbs
+ */
 router.get("/", proverbsController.getAllProverbs);
+
+/**
+ * @swagger
+ * /proverbs/{id}:
+ *   get:
+ *     summary: Retrieve a proverb by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The proverb ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved proverb
+ */
 router.get("/:id", proverbsController.getSingleProverb);
+
+/**
+ * @swagger
+ * /proverbs:
+ *   post:
+ *     summary: Create a new proverb
+ *     description: Adds a proverb to the database
+ *     responses:
+ *       201:
+ *         description: Proverb created successfully
+ *       400:
+ *         description: Invalid input
+ */
 router.post("/", proverbsController.createProverb);
+
+/**
+ * @swagger
+ * /proverbs/{id}:
+ *   put:
+ *     summary: Update a proverb
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the proverb to update
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Proverb updated successfully
+ */
 router.put("/:id", proverbsController.updateProverb);
+
+/**
+ * @swagger
+ * /proverbs/{id}:
+ *   delete:
+ *     summary: Delete a proverb
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the proverb to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Proverb deleted successfully
+ */
 router.delete("/:id", proverbsController.deleteProverb);
 
 module.exports = router;
--- routes/tribes.js
+++ routes/tribes.js
@@ -1,14 +1,86 @@
-
 const express = require("express");
 const router = express.Router();
 
 const tribesController = require("../controllers/tribes");
 
+/**
+ * @swagger
+ * /tribes:
+ *   get:
+ *     summary: Retrieve all Nigerian tribes
+ *     description: Returns a list of tribes stored in the database
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved tribes
+ */
 router.get("/", tribesController.getAllTribes);
+
+/**
+ * @swagger
+ * /tribes/{id}:
+ *   get:
+ *     summary: Retrieve a tribe by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The tribe ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved tribe
+ */
 router.get("/:id", tribesController.getSingleTribe);
+
+/**
+ * @swagger
+ * /tribes:
+ *   post:
+ *     summary: Create a new tribe
+ *     description: Adds a tribe to the database
+ *     responses:
+ *       201:
+ *         description: Tribe created successfully
+ *       400:
+ *         description: Invalid input
+ */
 router.post("/", tribesController.createTribe);
+
+/**
+ * @swagger
+ * /tribes/{id}:
+ *   put:
+ *     summary: Update a tribe
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the tribe to update
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Tribe updated successfully
+ */
 router.put("/:id", tribesController.updateTribe);
+
+/**
+ * @swagger
+ * /tribes/{id}:
+ *   delete:
+ *     summary: Delete a tribe
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the tribe to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Tribe deleted successfully
+ */
 router.delete("/:id", tribesController.deleteTribe);
 
 module.exports = router;
-
