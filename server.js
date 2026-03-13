2c30a7c1b7fdc12c61e5e97de224ff18790653fe
Morris Oliha <niyemayo@gmail.com>
2026-03-13 12:21:51 +0100

    Fix API routes with /api prefix

--- server.js
+++ server.js
@@ -1,35 +1,29 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();   
-
 const mongodb = require("./db/connect");
-
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger");
+const swaggerDocument = require("./swagger.json");
 
-const proverbsRoutes = require("./routes/proverbs");
-const tribesRoutes = require("./routes/tribes");
-
+const app = express();
 const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
 
-// Swagger documentation
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument, { explorer: true })
-);
-app.get("/", (req, res) => {
-  res.send("Nigerian Proverbs API is running");
-});
-// Routes
-app.use("/proverbs", proverbsRoutes);
-app.use("/tribes", tribesRoutes);
+// Swagger Documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Database connection
+// Routes
+app.use("/api/proverbs", require("./routes/proverbs"));
+app.use("/api/tribes", require("./routes/tribes"));
+
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Nigerian Proverbs API");
+});
+
+// Connect to MongoDB and start server
 mongodb.initDb((err) => {
   if (err) {
     console.log(err);
