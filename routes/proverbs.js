3a6dd231b8b49166e526219f372232ecafbbbf8b
Morris Oliha <niyemayo@gmail.com>
2026-03-14 01:25:27 +0100

    Fix route handler

--- routes/proverbs.js
+++ routes/proverbs.js
@@ -3,19 +3,14 @@ const router = express.Router();
 
 const proverbsController = require("../controllers/proverbs");
 
-/* GET all proverbs */
 router.get("/", proverbsController.getAll);
 
-/* GET single proverb */
 router.get("/:id", proverbsController.getSingle);
 
-/* CREATE proverb */
 router.post("/", proverbsController.createProverb);
 
-/* UPDATE proverb */
 router.put("/:id", proverbsController.updateProverb);
 
-/* DELETE proverb */
 router.delete("/:id", proverbsController.deleteProverb);
 
 module.exports = router;
