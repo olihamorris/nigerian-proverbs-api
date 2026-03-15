0dc3dff2839afe75384d525bd6266a648e4a2967
Morris Oliha <niyemayo@gmail.com>
2026-03-15 07:18:41 +0100

    Fix tribe router

--- routes/tribes.js
+++ routes/tribes.js
@@ -3,10 +3,19 @@ const router = express.Router();
 
 const tribesController = require("../controllers/tribes");
 
+// GET all tribes
 router.get("/", tribesController.getAll);
+
+// GET a single tribe
 router.get("/:id", tribesController.getSingle);
+
+// POST create a tribe
 router.post("/", tribesController.createTribe);
+
+// PUT update a tribe
 router.put("/:id", tribesController.updateTribe);
+
+// DELETE a tribe
 router.delete("/:id", tribesController.deleteTribe);
 
 module.exports = router;
