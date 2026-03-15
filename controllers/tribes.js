a6170e049ce02073a8772228630db5d2f285706c
Morris Oliha <niyemayo@gmail.com>
2026-03-15 07:24:44 +0100

    Fix controllers/tribes

--- controllers/tribes.js
+++ controllers/tribes.js
@@ -1,150 +1,97 @@
 const mongodb = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
-
-// GET all tribes
-const getAllTribes = async (req, res) => {
+const getAll = async (req, res) => {
   try {
-    const result = await mongodb.getDb().collection("tribes").find();
+    const result = await mongodb.getDb().db().collection("tribes").find();
     const tribes = await result.toArray();
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(tribes);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
-
-// GET single tribe
-const getSingleTribe = async (req, res) => {
+const getSingle = async (req, res) => {
   try {
     const tribeId = new ObjectId(req.params.id);
-
-    const tribe = await mongodb
-      .getDb()
-      .collection("tribes")
-      .findOne({ _id: tribeId });
-
-    res.status(200).json(tribe);
-  } catch (error) {
-    res.status(500).json(error);
+    const result = await mongodb.getDb().db().collection("tribes").find({ _id: tribeId });
+    const tribe = await result.toArray();
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(tribe[0]);
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
-
-// CREATE tribe
 const createTribe = async (req, res) => {
   try {
-
-    if (
-      !req.body.tribe ||
-      !req.body.region ||
-      !req.body.language ||
-      !req.body.population ||
-      !req.body.famousProverb ||
-      !req.body.provider ||
-      !req.body.date
-    ) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
     const tribe = {
-      tribe: req.body.tribe,
+      name: req.body.name,
       region: req.body.region,
       language: req.body.language,
       population: req.body.population,
-      famousProverb: req.body.famousProverb,
+      culture: req.body.culture,
       provider: req.body.provider,
       date: req.body.date
     };
 
-    const response = await mongodb
-      .getDb()
-      .collection("tribes")
-      .insertOne(tribe);
+    const response = await mongodb.getDb().db().collection("tribes").insertOne(tribe);
 
     if (response.acknowledged) {
-      res.status(201).json(response);
+      res.status(201).send();
     } else {
-      res.status(500).json(response.error);
+      res.status(500).json(response.error || "Error creating tribe");
     }
-
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
-
-// UPDATE tribe
 const updateTribe = async (req, res) => {
   try {
-
-    if (
-      !req.body.tribe ||
-      !req.body.region ||
-      !req.body.language ||
-      !req.body.population ||
-      !req.body.famousProverb ||
-      !req.body.provider ||
-      !req.body.date
-    ) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
     const tribeId = new ObjectId(req.params.id);
 
     const tribe = {
-      tribe: req.body.tribe,
+      name: req.body.name,
       region: req.body.region,
       language: req.body.language,
       population: req.body.population,
-      famousProverb: req.body.famousProverb,
+      culture: req.body.culture,
       provider: req.body.provider,
       date: req.body.date
     };
 
-    const response = await mongodb
-      .getDb()
-      .collection("tribes")
-      .replaceOne({ _id: tribeId }, tribe);
+    const response = await mongodb.getDb().db().collection("tribes").replaceOne({ _id: tribeId }, tribe);
 
     if (response.modifiedCount > 0) {
-      res.status(200).send();
-    } else {
-      res.status(500).json(response.error);
-    }
-
-  } catch (error) {
-    res.status(500).json(error);
-  }
-};
-
-
-// DELETE tribe
-const deleteTribe = async (req, res) => {
-  try {
-
-    const tribeId = new ObjectId(req.params.id);
-
-    const response = await mongodb
-      .getDb()
-      .collection("tribes")
-      .deleteOne({ _id: tribeId });
-
-    if (response.deletedCount > 0) {
       res.status(204).send();
     } else {
-      res.status(500).json(response.error);
+      res.status(500).json(response.error || "Error updating tribe");
     }
-
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
+const deleteTribe = async (req, res) => {
+  try {
+    const tribeId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection("tribes").deleteOne({ _id: tribeId });
+
+    if (response.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res.status(500).json(response.error || "Error deleting tribe");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 module.exports = {
-  getAllTribes,
-  getSingleTribe,
+  getAll,
+  getSingle,
   createTribe,
   updateTribe,
   deleteTribe
